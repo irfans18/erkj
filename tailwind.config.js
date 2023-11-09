d4f2fb2b78d9627414cc448721d4d247f74dc18c
irfans18 <nforspeed521@gmail.com>
2023-11-09 13:13:29 +0700

    Remove Item 4 & 5. Reconfigure flowbite.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	// content: ["*.{html,js}"],
-	content: ["*.{html,js}", "./node_modules/flowbite/**/*.js"],
+	content: ["*.{html,js}", "./include/js/flowbite/**/*.js"],
 	theme: {
 		extend: {
 			colors: {
