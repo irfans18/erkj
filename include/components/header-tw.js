2af0abbda7236e051cfd75ddee7310845b431b9d
irfans18 <nforspeed521@gmail.com>
2024-07-14 18:14:32 +0700

    improve: :rocket: :art: about us

--- include/components/header-tw.js
+++ include/components/header-tw.js
@@ -26,97 +26,52 @@ headerTemplate.innerHTML = `
          background-color: white;
       }
    </style>
-   <header
-      class="navbar-1-5 max-h-10"
-      style="font-family: 'Poppins', sans-serif"
-   >
-      <div
-         class="container mx-auto flex flex-wrap flex-row items-center justify-between"
-      >
-         <nav
-            class="lg:flex hidden lg:w-2/5 flex-wrap items-center text-base md:ml-auto lg:space-y-0 space-y-6 lg:space-x-2 space-x-0"
-         >
-            <a
-               href="index.html"
-               class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link"
-
-               >Beranda</a
-            >
-            <a
-               href="index.html#catalog"
-               class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link"
-               >Produk</a
-            >
-            <a
-               target="_blank"
-               href="https://api.whatsapp.com/send/?phone=6282221045069&text=Halo+Rumah+Keju+Jogja+%2C+Saya+ingin+bertanya+seputar+pelatihan/edukasi+KEJUGJA+Artisan+Cheese&type=phone_number&app_absent=0"
-               class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link"
-               >Workshop</a
-            >
-            <a
-               href="about.html"
-               class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link"
-               >About Us</a
-            >
-         </nav>
-         <a
-            href="index.html"
-            class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-center"
-         >
-            <img src="assets/logo.png" width="120" alt="" />
-         </a>
-         <label for="menu-toggle" class="cursor-pointer lg:hidden block">
-            <svg
-               class="w-6 h-6"
-               fill="none"
-               stroke="#092A33"
-               viewBox="0 0 24 24"
-               xmlns="http://www.w3.org/2000/svg"
-            >
-               <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-               ></path>
-            </svg>
-         </label>
-         <input class="hidden" type="checkbox" id="menu-toggle" />
-         <div
-            class="hidden w-full flex-wrap items-center text-base justify-center"
-            id="menu"
-         >
-            <nav
-               class="text-center lg:space-x-12 space-x-0 lg:flex items-center justify-between text-base pt-8 lg:pt-0 lg:space-y-0 space-y-6"
-            >
-               <a href="index.html" class="nav-link block active"
-                  >Beranda</a
-               >
-               <a href="index.html#catalog" class="nav-link block"
-                  >Produk</a
-               >
-               <a
-                  href="https://api.whatsapp.com/send?phone=6289670155097&text=Hallo kak. Saya [nama] tertarik dengan kejugja, Boleh minta info lebih lanjut terkait produk/workshop?"
-                  class="nav-link block"
-                  >Workshop</a
-               >
-               <a href="about.html" class="nav-link block">About Us</a>
-            </nav>
-         </div>
-         <div
-            class="lg:w-2/5 lg:inline-flex text-center py-4 lg:justify-end ml-0 hidden lg:items-center w-full"
-            id="menu"
-         >
-            <a
-               target="_blank"
-               href="https://api.whatsapp.com/send?phone=6289670155097&text=Hallo kak. Saya [nama] tertarik dengan kejugja, Boleh minta info lebih lanjut terkait produk/workshop?"
-               class="bg-orange-rkj hover:bg-orange-400 text-white items-center border-0 py-3 px-8 focus:outline-none rounded-2xl font-medium text-base mt-6 lg:mt-0"
-            >
-               Contact Us
-            </a>
-         </div>
-      </div>
-   </header>
+   <header class="max-h-6 pb-12" style="font-family: 'Poppins', sans-serif">
+			<div class="container mx-auto flex flex-wrap flex-row items-center justify-between">
+				<nav
+					class="lg:flex hidden lg:w-2/5 flex-wrap items-center text-base md:ml-auto lg:space-y-0 space-y-6 lg:space-x-2 space-x-0">
+					<a href="index.html"
+						class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link">Beranda</a>
+					<a href="index.html#catalog"
+						class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link">Produk</a>
+					<a target="_blank"
+						href="https://api.whatsapp.com/send/?phone=6282221045069&text=Halo+Rumah+Keju+Jogja+%2C+Saya+ingin+bertanya+seputar+pelatihan/edukasi+KEJUGJA+Artisan+Cheese&type=phone_number&app_absent=0"
+						class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link">Workshop</a>
+					<a href="about.html"
+						class="text-orange-rkj hover:text-white hover:bg-orange-rkj rounded-lg p-0 lg:p-2 nav-link">About
+						Us</a>
+				</nav>
+				<a href="index.html"
+					class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-center">
+					<img src="assets/logo.png" width="120" alt="" />
+				</a>
+				<label for="menu-toggle" class="cursor-pointer lg:hidden block">
+					<svg class="w-6 h-6" fill="none" stroke="#092A33" viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"></path>
+					</svg>
+				</label>
+				<input class="hidden" type="checkbox" id="menu-toggle" />
+				<div class="hidden w-full flex-wrap items-center text-base justify-center" id="menu">
+					<nav
+						class="text-center lg:space-x-12 space-x-0 lg:flex items-center justify-between text-base pt-8 lg:pt-0 lg:space-y-0 space-y-6">
+						<a href="index.html" class="nav-link block active">Beranda</a>
+						<a href="index.html#catalog" class="nav-link block">Produk</a>
+						<a href="https://api.whatsapp.com/send?phone=6289670155097&text=Hallo kak. Saya [nama] tertarik dengan kejugja, Boleh minta info lebih lanjut terkait produk/workshop?"
+							class="nav-link block">Workshop</a>
+						<a href="about.html" class="nav-link block">About Us</a>
+					</nav>
+				</div>
+				<div class="lg:w-2/5 lg:inline-flex text-center py-4 lg:justify-end ml-0 hidden lg:items-center w-full"
+					id="menu">
+					<a href="mailto:rumahkejujogja@gmail.com"
+						class="bg-orange-rkj hover:bg-orange-400 text-white items-center border-0 py-3 px-8 focus:outline-none rounded-2xl font-medium text-base mt-6 lg:mt-0">
+						Contact Us
+					</a>
+				</div>
+			</div>
+		</header>
 `;
 
 class Header extends HTMLElement {
